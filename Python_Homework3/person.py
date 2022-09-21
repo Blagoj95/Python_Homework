e6b1f3c38e5974aaac2dffd332e1684fa7d23959
Blagoj95 <vaziskib@gmail.com>
2022-09-21 16:13:38 +0200

    Adding Python_homework3

---
+++ Python_Homework3/person.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self,fname,lname,email,mobnumb):
+         self.firstname = str(fname)
+         self.lastname = str(lname)
+         self.email = str(email)
+         self.mobilenumber = int(mobnumb)
+
+   
---
+++ Python_Homework3/student.py
@@ -0,0 +1,35 @@
+from person import Person
+from email_validator import validate_email, EmailSyntaxError
+
+class Student(Person):
+    def __init__(self, fname, lname, email, mobnumb):
+        super().__init__(fname, lname, email, mobnumb)
+       
+        
+    def info(self):
+        print(f"Person:\n First name: {self.firstname}\n Last name: {self.lastname}\n "
+             f"Email:{self.email}\n Mobile: {self.mobilenumber}\n"
+             f"Is a Student of the following courses and has grades")
+        courses =  {
+            "Git":"7",
+            "Python":8,
+            "AWS":10,
+            "Docker":3,
+            "Linux":4
+            }
+        count = 1 
+        for course in courses:
+            print(f"{count}.{course}:{courses[course]}")
+            count+=1
+
+
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+email = input("Enter your email: ")
+try:
+    email = validate_email(email).email
+except EmailSyntaxError as e:
+    print(str(e))
+number = input("Enter your phone number: ")
+x = Student(name,surname,email,number)
+x.info()
